b5773d5059765a422a7b3d06b54419bc22350249
kapila-silwathge <kapila_silwathge@yahoo.com>
2020-05-02 21:04:31 +0530

    fixing anauthorized redirection not working with BehaviorSubject working with

--- src/app/basic-auth-interceptor.service.ts
+++ src/app/basic-auth-interceptor.service.ts
@@ -11,9 +11,9 @@ export class BasicAuthInterceptorService implements HttpInterceptor {
     constructor(private authenticationService: AuthenticationService ,
     private router: Router) { }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (this.authenticationService.isUserLoggedIn() && req.url.indexOf('basicauth') === -1) {
-            const authReq = req.clone({
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {    
+        if (this.authenticationService.username && req.url.indexOf('basicauth') === -1) {
+         	const authReq = req.clone({
                 headers: new HttpHeaders({
                     'Content-Type': 'application/json',
                     'Authorization': `Basic ${window.btoa(this.authenticationService.username + ":" + this.authenticationService.password)}`
@@ -21,14 +21,13 @@ export class BasicAuthInterceptorService implements HttpInterceptor {
             });
             return next.handle(authReq);
         } else {
-            /*return next.handle(req);*/
             return next.handle(req).pipe( tap(() => {},
 		      (err: any) => {
 		      if (err instanceof HttpErrorResponse) {
 		        if (err.status !== 401) {
 		         return;
-		        }
-		        this.router.navigate(['login']);
+		        }		        
+		        this.router.navigate(['/login']);
 		      }
 		    }));
         }
